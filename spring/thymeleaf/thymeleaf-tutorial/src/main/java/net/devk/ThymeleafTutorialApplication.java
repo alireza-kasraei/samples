ab0a23eb6d0ba5f810b14d6eff3cd729efb0c821
alireza-kasraei <kasraei.ar@gmail.com>
2017-05-08 22:08:25 +0100

    thymeleaf tutorial project created

---
+++ spring/thymeleaf/thymeleaf-tutorial/src/main/java/net/devk/ThymeleafTutorialApplication.java
@@ -0,0 +1,12 @@
+package net.devk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTutorialApplication.class, args);
+	}
+}
